3e4687cb3eab548e8b8aaa1966ba2c8647020f23
rizki87 <rizkipratama87@gmail.com>
2021-08-12 06:46:15 +0700

    firebase admin with nextjs + swr

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
 import '../styles/globals.css'
+import { AuthProvider } from '@/lib/auth'
+import { CSSReset, ThemeProvider } from '@chakra-ui/react'
+import { Global, css } from '@emotion/react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import customTheme from '@/styles/theme'
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={customTheme}>
+    <AuthProvider>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </AuthProvider>
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default App
